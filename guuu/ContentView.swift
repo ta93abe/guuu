d72113eb6f0ba053a1807825f1ebe202e25bed27
Takumi Abe <ta93psymagi@gmail.com>
2020-05-22 18:01:14 +0900

    Initial Commit

---
+++ guuu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guuu
+//
+//  Created by Takumi Abe on 2020/05/22.
+//  Copyright © 2020 Takumi Abe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
